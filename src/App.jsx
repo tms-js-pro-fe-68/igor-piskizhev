578530ed4ba438df7552c38ef05e147aecb19057
Igor Piskizhev <zybrec07@gmail.com>
2022-03-03 21:11:53 +0300

    eslint+prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App({ prop1 }) {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <header className="App-header">
+        {prop1}
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount( c => c + 1)}>
             count is: {count}
           </button>
         </p>
