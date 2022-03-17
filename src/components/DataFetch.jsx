9d631182186838fd7c37580c50b2bb1eb1a50b16
Igor Piskizhev <zybrec07@gmail.com>
2022-03-17 15:07:21 +0300

    fixCheckStatus

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -7,13 +7,18 @@ export default function GetStat(){
 
    useEffect(() => {
     setLoading(true)
-    fetch("https://api.chucknorris.io/jokes/random")
-    .then(response => response.json())
-    .then(data => {
-        setChackFact(arr => [...arr,data]);
-        setLoading(false);
-    })
-    .catch(error => console.error(error))
+    async function getText() {
+        const response = await fetch("https://api.chucknorris.io/jokes/random");
+        if (response.status === 200) {
+            response.json()
+            .then(data => {
+                setChackFact(arr => [...arr,data]);
+                setLoading(false);
+            })
+            .catch(error => console.error(error))
+        }
+    }
+    getText()
    } ,[count])
    return (
     <>
@@ -30,7 +35,7 @@ export default function GetStat(){
         <div key={fact.id}
         style = {{
                 background:'rgba(255, 88, 42, 0.5)',
-                color: 'white',
+                color: 'red',
                 fontWeight: '180',
                 margin: '10px',
                 padding: '10px',
